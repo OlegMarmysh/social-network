fc6ec55392e5d2df12bf0c783707c9bcde485eb1
Bbara <backdown959@gmail.com>
2019-12-10 22:40:01 +0300

    Updates

---
+++ precache-manifest.494f5faceb4150da342680d156a43f0c.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f8aea4a3d49042b0ec0e657215eef5d",
+    "url": "/OlegMarmysh/social-network/index.html"
+  },
+  {
+    "revision": "a562f057d6744bf57da6",
+    "url": "/OlegMarmysh/social-network/static/css/3.43d95cfc.chunk.css"
+  },
+  {
+    "revision": "42eef8ccb98bf64936db",
+    "url": "/OlegMarmysh/social-network/static/css/4.ea2c7622.chunk.css"
+  },
+  {
+    "revision": "a0285cd3dfee0d6b4bd3",
+    "url": "/OlegMarmysh/social-network/static/css/main.35865d94.chunk.css"
+  },
+  {
+    "revision": "618a8cf887043d22d8db",
+    "url": "/OlegMarmysh/social-network/static/js/2.aa7d9371.chunk.js"
+  },
+  {
+    "revision": "a562f057d6744bf57da6",
+    "url": "/OlegMarmysh/social-network/static/js/3.bc158dce.chunk.js"
+  },
+  {
+    "revision": "42eef8ccb98bf64936db",
+    "url": "/OlegMarmysh/social-network/static/js/4.fb87add9.chunk.js"
+  },
+  {
+    "revision": "a0285cd3dfee0d6b4bd3",
+    "url": "/OlegMarmysh/social-network/static/js/main.dc912af1.chunk.js"
+  },
+  {
+    "revision": "8f25e7fb12dc6b9db221",
+    "url": "/OlegMarmysh/social-network/static/js/runtime~main.958a058b.js"
+  },
+  {
+    "revision": "b0fb26613e6b038a17e61144d1aa65ab",
+    "url": "/OlegMarmysh/social-network/static/media/Spinner-1s-200px.b0fb2661.svg"
+  },
+  {
+    "revision": "139885546ce6572b4b6b8712c143afd0",
+    "url": "/OlegMarmysh/social-network/static/media/users-vector-icon-png_260862.13988554.jpg"
+  }
+]);
