aac42daf8d43a5cf64bd83389a7b779fc5938e9a
Bbara <backdown959@gmail.com>
2019-12-10 21:16:38 +0300

    'add_suspense'

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-import React, {Component} from 'react';
+import React, {Component,Suspense} from 'react';
 import './App.css';
 import Navbar from "./components/Navbar/NavBar";
 import {BrowserRouter, Route, withRouter} from "react-router-dom";
 import News from "./components/News/News";
 import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
-import DialogsContainer from "./components/Dialogs/DialogsContainer";
 import UsersContainer from "./components/Users/UsersContainer";
-import ProfileContainer from "./components/Profil/ProfileContainer";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import Login from "./components/Login/Login";
 import {connect, Provider} from "react-redux";
@@ -15,6 +13,9 @@ import {compose} from "redux";
 import {initializeApp} from "./redux/appReducer";
 import Preloader from "./components/Common/Preloader/Preloader";
 import store from "./redux/reduxStore";
+import {withSuspense} from "./hoc/withSuspense";
+const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'));
+const ProfileContainer = React.lazy(() => import('./components/Profil/ProfileContainer'));
 
 class App extends Component {
     componentDidMount() {
@@ -30,8 +31,8 @@ class App extends Component {
                     <HeaderContainer/>
                     <Navbar/>
                     <div className='app_wrapper_profile'>
-                        <Route path='/profile/:userId?' render={() => <ProfileContainer/>}/>
-                        <Route path='/dialogs' render={() => <DialogsContainer/>}/>
+                        <Route path='/profile/:userId?' render={withSuspense(ProfileContainer)}/>
+                        <Route path='/dialogs' render={withSuspense(DialogsContainer)}/>
                         <Route path='/news' render={() => <News/>}/>
                         <Route path='/music' render={() => <Music/>}/>
                         <Route path='/settings' render={() => <Settings/>}/>
