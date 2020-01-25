84a9ebc28a8ad534e42923dcafbe3f9d4102e62b
Bbara <backdown959@gmail.com>
2020-01-25 16:13:53 +0300

    stylization profile final

--- src/components/Profil/Description/ProfileDataReduxForm.js
+++ src/components/Profil/Description/ProfileDataReduxForm.js
@@ -1,29 +1,29 @@
 import React from "react";
 import {reduxForm} from "redux-form";
 import {createField, Input, Textarea} from "../../Common/FormsControl";
-import s from './Description.module.css';
+import s from './ProfileDataReduxForm.module.css';
 import styles from "../../Common/FormsControl.module.css";
 
 const ProfileDataForm = ({handleSubmit, profile, error}) => {
-    return <div>
+    return <div className={s.content}>
         <form onSubmit={handleSubmit}>
-            <div>
-                <button>save</button>
-            </div>
             {error && <div className={styles.loginError}>
                 {error}
             </div>}
             <div>
-                <b>Full Name: </b> {createField('fullName', 'Full Name', Input)}
+                <b>Full Name: </b> {createField('fullName', 'Full Name', Input, null, {type: 'text'})}
             </div>
             <div>
-                <b>About me: </b> {createField('aboutMe', 'About me', Textarea)}
+                <b>About me: </b> {createField('aboutMe', 'About me', Input, null, {type: 'text'})}
             </div>
-            <div>
+            <div className={s.lookForJob}>
                 <b>Looking for a job: </b> {createField('lookingForAJob', null, Input, null, {type: 'checkbox'})}
             </div>
             <b>Looking for a job
-                description: </b> {createField('lookingForAJobDescription', 'Looking for a job description', Textarea)}
+                description: </b> {createField('lookingForAJobDescription', 'Looking for a job description', Input, null, {type: 'text'})}
+            <div>
+                <button>save</button>
+            </div>
             {/*<div>
                 <b>Contacts: </b>{Object.keys(profile.contacts).map(key => {
                 return <div key={key} className={s.contact}>
